377f89e43193ddd36e43ca12e9899087e0aaa9eb
bobbingboy <zo17751991.cm03g@g2.nctu.edu.tw>
2026-09-09 00:16:54 +0800

    Add: string-encode-and-decode - submission-12

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-12.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for (String str : strs) {
+            int length = str.length();
+            sb.append(length).append("#").append(str);
+        }
+        return sb.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> result = new ArrayList<>();
+        int index = 0;
+        while (index < str.length()) {
+            int targetSeparateSymbolIndex = str.indexOf('#', index);
+            int length =
+                Integer.parseInt(str.substring(index, targetSeparateSymbolIndex));
+
+            int start = targetSeparateSymbolIndex + 1;
+            int end = start + length;
+            result.add(str.substring(start, end));
+            index = end;
+        }
+        return result;
+    }
+}
